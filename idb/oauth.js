2fc20b1d7afb197049ba9ab26329dbe228aa637c
Sathish Kumar <sathish02infiniti@gmail.com>
2021-08-14 18:23:06 +0530

    main

--- idb/oauth.js
+++ idb/oauth.js
@@ -1,24 +1,4 @@
 
 
 
-function onSignIn(googleUser) {
-
-    var profile = googleUser.getBasicProfile();
-    var uname = profile.getName();
-    var umail = profile.getEmail();
-    var uimg = profile.getImageUrl()
-
-    var splitmail = umail;
-    var mailword = splitmail.substring(0, splitmail.lastIndexOf("@"));
-
-    database.ref("/users/"+mailword+"/umail").set(umail);
-    database.ref("/users/"+mailword+"/uname").set(uname);
-    database.ref("/users/"+mailword+"/upass").set(uimg);
-
-    console.log(" User successfully created! .. Try Login ");
-
-    window.location.href = 'index.html';
-    
-}
-
 
