8cee77e90a4ff8cac03972d408e04938d39b2c27
Sathish Kumar <sathish02infiniti@gmail.com>
2021-08-14 18:18:28 +0530

    main

--- idb/oauth.js
+++ idb/oauth.js
@@ -1,22 +1,15 @@
 
-  var database = firebase.database();
+
 
 function onSignIn(googleUser) {
+
     var profile = googleUser.getBasicProfile();
     var uname = profile.getName();
     var umail = profile.getEmail();
     var uimg = profile.getImageUrl()
-    $("#name").text(uname);
-    $("#email").text(umail);
-    $("#image").attr('src', uimg);
-    $(".data").css("display", "block");
-    //$(".g-signin2").css("display", "none");
 
     var splitmail = umail;
-    console.log(splitmail)
     var mailword = splitmail.substring(0, splitmail.lastIndexOf("@"));
-
-    console.log(mailword)
 
     database.ref("/users/"+mailword+"/umail").set(umail);
     database.ref("/users/"+mailword+"/uname").set(uname);
@@ -27,13 +20,5 @@ function onSignIn(googleUser) {
     window.location.href = 'index.html';
     
 }
-console.log("out");
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        alert("You have been signed out successfully");
-        $(".data").css("display", "none");
-        $(".g-signin2").css("display", "block");
-    });
-}
+
