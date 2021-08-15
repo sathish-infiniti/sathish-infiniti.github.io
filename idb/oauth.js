c656834b6347e59cd618ff62c906adcab8c0aa33
Sathish Kumar <sathish02infiniti@gmail.com>
2021-08-15 09:25:24 +0530

    main

--- idb/oauth.js
+++ idb/oauth.js
@@ -1,34 +1,31 @@
-function onSignIn(googleUser) {
+
+function onSignIn(googleUser) 
+{
 
     var profile = googleUser.getBasicProfile();
     var uname = profile.getName();
     var umail = profile.getEmail();
     var uimg = profile.getImageUrl();
-    //var profile = profile;
-  
+
     var splitmail = umail;
     var mailword = splitmail.substring(0, splitmail.lastIndexOf("@"));
 
     database.ref("/users/"+mailword+"/umail").set(umail);
     database.ref("/users/"+mailword+"/uname").set(uname);
     database.ref("/users/"+mailword+"/uimg").set(uimg);
-    //database.ref("/users/"+mailword+"/profile").set(profile);
 
-    console.log(" User successfully created! .. Try Login ");
 
-    if (typeof(Storage) !== "undefined") 
-    {
-        // Code for localStorage/sessionStorage.
+    // if (typeof(Storage) !== "undefined") 
+    // {
         localStorage.setItem("umail", umail);
         localStorage.setItem("uname", uname);
         localStorage.setItem("uimg", uimg);
-    } 
-    else 
-    {
-        // Sorry! No Web Storage support..
-        alert("Browser Not Supported");
-    }
+    // } 
+    // else 
+    // {
+      // alert("Browser Not Supported");
+    // }
 
-    window.location.href = 'index.html';
+        window.location.href = 'index.html';
     
-  }
+}
