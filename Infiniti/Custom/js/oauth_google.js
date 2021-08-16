a230bc59134e9438efd3fc6842c81f9330781d11
Sathish Kumar <sathish02infiniti@gmail.com>
2021-08-17 01:47:01 +0530

    main

--- Infiniti/Custom/js/oauth_google.js
+++ Infiniti/Custom/js/oauth_google.js
@@ -1,28 +1,33 @@
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    var uname = profile.getName();
-    var umail = profile.getEmail();
-    var uimg = profile.getImageUrl();
+  var profile = googleUser.getBasicProfile();
+  var uname = profile.getName();
+  var umail = profile.getEmail();
+  var uimg = profile.getImageUrl();
 
-    var splitmail = umail;
-    var mailword = splitmail.substring(0, splitmail.lastIndexOf("@"));
+  var splitmail = umail;
+  var mailword = splitmail.substring(0, splitmail.lastIndexOf("@"));
 
-    database.ref("/users/"+mailword+"/umail").set(umail);
-    database.ref("/users/"+mailword+"/uname").set(uname);
-    database.ref("/users/"+mailword+"/uimg").set(uimg);
+  database.ref("/users/"+mailword+"/umail").set(umail);
+  database.ref("/users/"+mailword+"/uname").set(uname);
+  database.ref("/users/"+mailword+"/uimg").set(uimg);
 
-    // if (typeof(Storage) !== "undefined") 
-    // {
-        localStorage.setItem("oauth_umail", umail);
-        localStorage.setItem("oauth_uname", uname);
-        localStorage.setItem("oauth_uimg", uimg);
-    // } 
-    // else 
-    // {
-      // alert("Browser Not Supported");
-    // }
+  // if (typeof(Storage) !== "undefined") 
+  // {
+      localStorage.setItem("oauth_umail", umail);
+      localStorage.setItem("oauth_user_name", uname);
+      localStorage.setItem("oauth_user_image", uimg);
 
-        window.location.href = 'index.html';
+      console.log(uname);
+      console.log(umail);
+      console.log(uimg);
+
+  // } 
+  // else 
+  // {
+    // alert("Browser Not Supported");
+  // }
+
+      window.location.href = 'index.html';
 }
 
 
